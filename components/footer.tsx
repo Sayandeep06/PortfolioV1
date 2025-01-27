3eee8ebeed1104dccc1d6f292ced710a3a03b3df
Sayandeep Dey <sayandeepdeyimp@gmail.com>
2025-01-28 04:05:37 +0530

    footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
       </small>
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with
-        React & Next.js, TypeScript, Tailwind CSS,
+        React & Next.js, TypeScript, and Tailwind CSS
       </p>
     </footer>
   );
